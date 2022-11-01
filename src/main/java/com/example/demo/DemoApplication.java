83c3415e88aba33086fc4557d8e4a274922447c2
Alberto Kimichima <alberto@pandoratecnologia.com.br>
2022-11-01 16:11:57 -0300

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,10 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	@GetMapping
-	public String home(HttpServletRequest request, @RequestBody String body) {
-		System.out.println(request.toString());
-		System.out.println(body);
-		return "Hello Docker World";
+	public ResponseEntity<?> home(HttpServletRequest request, @RequestParam String mode, @RequestParam String challenge, @RequestParam String token) {
+		System.out.println("mode: " + mode + " - challenge: " + challenge + "verify_token: " + token);
+		return ResponseEntity.ok().build();
 	}
 
 	public static void main(String[] args) {
