ab0a6ecfcb4ed208727ea882b2fa493827be768e
Alberto Kimichima <alberto@pandoratecnologia.com.br>
2022-11-01 16:17:26 -0300

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	@GetMapping
-	public ResponseEntity<?> home(HttpServletRequest request, @RequestParam String mode, @RequestParam String challenge, @RequestParam String token) {
-		System.out.println("mode: " + mode + " - challenge: " + challenge + "verify_token: " + token);
-		return ResponseEntity.ok().build();
+	public ResponseEntity<String> home(HttpServletRequest request, @RequestHeader Map<String,String> hub) {
+		System.out.println("mode: " + hub.get("mode") + " - challenge: " + hub.get("challenge") + "verify_token: " + hub.get("verify_token"));
+		return ResponseEntity.ok("demo");
 	}
 
 	public static void main(String[] args) {
