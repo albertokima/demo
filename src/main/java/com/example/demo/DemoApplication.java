ec01aa82cb493515167a35ad4f955ca5e74594a6
Alberto Lyoiti Kimichima <alberto@pandoratecnologia.com.br>
2022-11-25 16:55:28 -0300

    .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.whatsapp.WhatsappClient;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 @SpringBootApplication
 @RestController
@@ -23,12 +26,12 @@ public class DemoApplication {
 
 	@Autowired
 	private WhatsappClient waba;
-
+	
 	private static final String verify_token = "demo";
 
 	@GetMapping("/webhook")
-	public ResponseEntity<String> validate(HttpServletRequest request, @RequestParam Map<String, String> hub) {
-		hub.entrySet().forEach(h -> {
+	public ResponseEntity<String> validate(HttpServletRequest request, @RequestParam Map<String,String> hub) {
+		hub.entrySet().forEach(h->{
 			System.out.println(h.getKey() + ":" + h.getValue());
 		});
 
@@ -36,10 +39,10 @@ public class DemoApplication {
 		String mode = hub.get("hub.mode");
 		String token = hub.get("hub.verify_token");
 		String challenge = hub.get("hub.challenge");
-
+		
 		// Check if a token and mode were sent
 		if (mode != null && token != null) {
-			// Check the mode and token sent are correct
+		    // Check the mode and token sent are correct
 			if (mode.equals("subscribe") && token.equals(verify_token)) {
 				// Respond with 200 OK and challenge token from the request
 				System.out.println("WEBHOOK_VERIFIED");
@@ -47,23 +50,26 @@ public class DemoApplication {
 			} else {
 				// Responds with '403 Forbidden' if verify tokens do not match
 				return ResponseEntity.status(403).build();
-			}
+		    }
 		}
 		return ResponseEntity.badRequest().build();
 	}
 
 	@PostMapping("/webhook")
 	public ResponseEntity<?> home(HttpServletRequest request, @RequestBody Object entry) {
-		try {
-			String[] names = JSONObject.getNames(entry);
-			for (String n : names) {
-				System.out.println(n);
+		try
+		{
+			Gson gson = new Gson();
+			for(Entry<String, JsonElement> json: gson.toJsonTree(entry).getAsJsonObject().entrySet()) {
+				System.out.println(json.getKey() + ":" + json.getValue());
 			}
-			//System.out.println(entry.toString());
-		} catch (Exception e) {
+			//System.out.println(entry);
+		}
+		catch(Exception e)
+		{
 			e.printStackTrace();
 			return ResponseEntity.badRequest().build();
-		}
+		}				
 		return ResponseEntity.ok().build();
 	}
 
@@ -71,7 +77,7 @@ public class DemoApplication {
 	public ResponseEntity<?> whatsapp(HttpServletRequest request, @RequestBody Object message) {
 		return ResponseEntity.ok(waba.enviarMensagem(message));
 	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
